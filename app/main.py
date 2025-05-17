2f223fa5414f4f96aa0b12f6087aa18f14292488
Chaitanya Tammineni <chaitanya@Chaitanyas-MacBook-Air.local>
2025-05-16 22:33:32 -0400

    add icon

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from app.api.routes import router as api_router
 from app.core.routes import router as core_router
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI(title="ClipURL")
 
 @app.get("/")
 def root():
     return {"message": "Welcome to ClipURL"}
+
+@app.get("/favicon.ico")
+async def favicon():
+    # app/media/favicon.ico
+    file_path = os.path.join("app/media", "favicon.ico")
+    return FileResponse(file_path, media_type="image/x-icon")
 
 # include api router
 app.include_router(api_router,  prefix="/api", tags=["api"])
